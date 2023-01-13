42af6c4b1ff6903a6039d419edfe71abe11f03b4
Billy Merchan <billymerchan@gmail.com>
2023-01-12 22:48:53 -0500

    dockerizing frontend

--- Libre-Server/backend/server.js
+++ Libre-Server/backend/server.js
@@ -5,15 +5,22 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+// app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+//const dbo = require("./db/conn");
+
+app.get("/", (req, res) => {
+  res.send("<h2>Welcome Friends</h2>");
+});
 
 app.listen(port, () => {
   // perform a database connection when server starts
+
+  /*
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
 
   });
+  */
   console.log(`Server is running on port: ${port}`);
 });
