90e0759631e2090c61c2c152d1388364c0a9382c
8illy <billymerchan@gmail.com>
2023-01-26 10:10:08 -0500

    Testing proper linter code

--- testingLintCI.cpp
+++ testingLintCI.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-int main() {
-   int badFunc(){
-    std::cout << "smh bad indents"
-int x = 0
-        std::cout << "Woah these indents r bad"
-       
-   }
+int main(){
+   std::cout << "Hello";
 }
