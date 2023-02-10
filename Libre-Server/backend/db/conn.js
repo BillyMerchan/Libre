fff7c2eec3d092cf16f038383f6ddd4c103f2a37
Daniela Liang <artaffu@gmail.com>
2023-02-10 17:40:27 -0500

    edited server, added conn file

---
+++ Libre-Server/backend/db/conn.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require("mongodb");
+const database = process.env.DATABASE;
+
+const client = new MongoClient(database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _database;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, database) {
+      // Verify we got a good "db" object
+      if (database)
+      {
+        _database = database.db("Libre");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _database;
+  },
+};
+
+
+
--- Libre-Server/backend/server.js
+++ Libre-Server/backend/server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
 
