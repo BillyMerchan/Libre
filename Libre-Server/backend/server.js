67da6558262f1618ba772f0fecdc7b7204a5976d
Daniela Liang <artaffu@gmail.com>
2023-02-10 16:24:04 -0500

    uncomment server

--- Libre-Server/backend/server.js
+++ Libre-Server/backend/server.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
-// app.use(require("./routes/record"));
+app.use(require("./routes/record"));
 // get driver connection
-//const dbo = require("./db/conn");
+const dbo = require("./db/conn");
 
 app.get("/", (req, res) => {
   res.send("<h2>Welcome Friends</h2>");
@@ -15,12 +16,9 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   // perform a database connection when server starts
-
-  /*
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-
   });
-  */
+  
   console.log(`Server is running on port: ${port}`);
 });
