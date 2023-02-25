6ac8cb2c49fc7164a7de89b2b0e70b679104922a
Daniela Liang <artaffu@gmail.com>
2023-02-24 20:09:11 -0500

    Fix module

--- Libre-Server/backend/modules/getCollection.js
+++ Libre-Server/backend/modules/getCollection.js
@@ -1,13 +1,17 @@
 const MongoClient = require('mongodb').MongoClient
 
-const db = require('./db/conn')
+const dbo = require('./db/conn')
 
 module.exports = {
     getCollection: async function (collectionName) {
-    const client = await MongoClient.connect(url)
-    const db = client.db(dbName)
-    const collection = db.collection(collectionName)
-    
-    return collection
+        let db_connect = dbo.getDb("Libre");
+        db_connect
+          .collection("roomData")
+          .find({})
+          .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+    return db_connect.collection
     }
 } 
