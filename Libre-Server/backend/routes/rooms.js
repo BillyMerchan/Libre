c060fa994928ea356a557cc7a95f3e0629e52c53
Daniela Liang <artaffu@gmail.com>
2023-03-21 16:33:35 -0400

    ran lint

--- Libre-Server/backend/routes/rooms.js
+++ Libre-Server/backend/routes/rooms.js
@@ -1,27 +1,19 @@
-const express = require("express");
-const Room = require("../modules/room");
-const router = express.Router();
+const express = require('express')
+const Room = require('../modules/room')
+const router = express.Router()
 
 // output list of all rooms
-router.get("/list", async (req, res) => {
-    try {
-        let rooms = await Room.find();
-        res.json({data: rooms});
-    } catch (err) {
-        throw err;
-    }
-});
+router.get('/list', async (req, res) => {
+  const rooms = await Room.find()
+  res.json({ data: rooms })
+})
 
 // output a single room by roomId
-router.get("/:roomId", async (req, res) => {
-    try {
-        let room = await Room.findOne({_id: req.params.roomId});
-        if (room) {
-            res.json({data: room});
-        }
-    } catch (err) {
-        throw err;
-    }
-});
+router.get('/:roomId', async (req, res) => {
+  const room = await Room.findOne({ _id: req.params.roomId })
+  if (room) {
+    res.json({ data: room })
+  }
+})
 
-module.exports = router;
+module.exports = router
