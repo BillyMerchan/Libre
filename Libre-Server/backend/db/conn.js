68b89d7b2e427545225c2e240d8682b5d840e970
Daniela Liang <artaffu@gmail.com>
2023-03-24 17:20:59 -0400

    Modified conn and server to work with mongoose

--- Libre-Server/backend/db/conn.js
+++ Libre-Server/backend/db/conn.js
@@ -1,40 +1,16 @@
-const { MongoClient } = require("mongodb");
-const database = process.env.DATABASE;
+const { Mongoose } = require("mongoose");
+const url = process.env.DATABASE;
 
-const client = new MongoClient(database);
-
-// const client = new MongoClient(database, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
- 
-var _database;
-
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, database) {
-    // Verify we got a good "db" object
-    if (database)
-    {
-      _database = database.db("Libre");
-      console.log("Successfully connected to MongoDB."); 
-
-      listDatabases(client);
-    }
-    return callback(err);
-    });
-  },
- 
-  getDb: function () {
-    return _database;
-  },
+var options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
+Mongoose.connect(url, options, (err) => {
+  if (err) {
+    console.log("Connection error.");
+  } else {
+    console.log("Connected to MongoDB.");
+  }
+});
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(`   ${db.name}`));
-  
-};
--- Libre-Server/backend/server.js
+++ Libre-Server/backend/server.js
@@ -7,17 +7,12 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 // get driver connection
-const dbo = require("./db/conn");
+const db = require("./db/conn");
 
 app.get("/", (req, res) => {
   res.send("<h2>Welcome Friends</h2>");
 });
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  
+app.listen(port, () => {  
   console.log(`Server is running on port: ${port}`);
 });
