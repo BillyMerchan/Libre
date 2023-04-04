341d3c9e81a837bfcd0a264b2d57662ed3c281f7
Daniela Liang <artaffu@gmail.com>
2023-04-04 16:57:17 -0400

    fixed router paths

--- Libre-Server/backend/routes/rooms.js
+++ Libre-Server/backend/routes/rooms.js
@@ -3,20 +3,25 @@ const Room = require('../db/room')
 const router = express.Router()
 
 // output list of all rooms
-router.get('/room/list', async (req, res) => {
-  const rooms = await Room.find()
-  res.json({ data: rooms })
+router.get('/list', async (req, res) => {
+  try {
+    const rooms = await Room.find()
+    res.status(201).json({ data: rooms })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
 })
 
 // output a single room by roomId
-router.get('/room/:roomId', async (req, res) => {
+router.get('/:roomId', async (req, res) => {
   const room = await Room.findOne({ _id: req.params.roomId })
   if (room) {
     res.json({ data: room })
   }
 })
 
-router.post('/room/add', (req, res, next) => {
+router.post('/add', (req, res, next) => {
   try {
     const room = {
       _id: req.body.id,
