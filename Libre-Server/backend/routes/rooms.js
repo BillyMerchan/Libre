da0c77872bb973b96f5133d5e18a7cc14e9583e0
Daniela Liang <artaffu@gmail.com>
2023-04-07 17:30:54 -0400

    Added delete and updateOccupancy routes

--- Libre-Server/backend/routes/rooms.js
+++ Libre-Server/backend/routes/rooms.js
@@ -6,31 +6,69 @@ const router = express.Router()
 router.get('/list', async (req, res) => {
   try {
     const rooms = await Room.find()
-    res.status(201).json({ data: rooms })
+    res.status(200).json({ data: rooms })
   } catch (err) {
     console.error(err)
     res.status(500).send('Server Error')
   }
 })
 
-// output a single room by roomId
-router.get('/:roomId', async (req, res) => {
-  const room = await Room.findOne({ _id: req.params.roomId })
+// output a single room by roomNumber
+router.get('/:roomNumber', async (req, res) => {
+  const room = await Room.findOne({ _id: req.params.roomNumber })
   if (room) {
     res.json({ data: room })
   }
 })
 
+// adds one room with parameters roomNumber, max_size, occupancy
+// should not need to be used after all rooms have been added
 router.post('/add', (req, res, next) => {
   try {
     const room = {
-      roomNumber: req.body.id,
-      max_size: req.body.max_occupancy,
-      occupancy: req.body.max_occupancy
+      roomNumber: req.body.roomNumber,
+      max_size: req.body.max_size,
+      occupancy: req.body.occupancy
     }
     const newRoom = Room.create(room)
 
-    res.status(201).json(newRoom)
+    res.status(201).json({ message: 'Room added', data: newRoom })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
+})
+
+// Delete a room by roomNumber
+router.delete('/delete/:roomNumber', async (req, res) => {
+  try {
+    const deletedRoom = await Room.findByIdAndDelete(req.params.roomNumber)
+
+    if (!deletedRoom) {
+      return res.status(404).json({ error: 'Room not found' })
+    }
+
+    res.status(200).json({ message: 'Room deleted successfully' })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
+})
+
+// updates occupancy to either true or false
+router.put('/updateOccupancy/:roomNumber', async (req, res) => {
+  try {
+    const updatedRoom = await Room.findOneAndUpdate(
+      { roomNumber: req.params.roomNumber },
+      { occupancy: req.body.occupancy },
+      { new: true }
+    )
+
+    if (!updatedRoom) {
+      return res.status(404).json({ error: 'Room not found' })
+    }
+
+    res.status(200).json({ message: 'Occupancy updated', data: updatedRoom })
   } catch (err) {
     console.error(err)
     res.status(500).send('Server Error')
