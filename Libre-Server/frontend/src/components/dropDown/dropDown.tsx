328efbf7756b47d27372b712aeeb352695f8994f
Billy Merchan <billymerchan@gmail.com>
2023-04-08 20:51:07 -0400

    Dropdown fixes

--- Libre-Server/frontend/src/components/dropDown/dropDown.tsx
+++ Libre-Server/frontend/src/components/dropDown/dropDown.tsx
@@ -2,22 +2,34 @@ import React, {FC} from 'react';
 import './dropDown.scss';
 
 const DropDown: FC = () => {
-    /*{
+    {
         const dropDowns = document.querySelectorAll('.dropDown');
         dropDowns.forEach(dropDown => {
-            const select = dropDown.querySelector('.select');
-            const caret = dropDown.querySelector('.caret');
-            const menu = dropDown.querySelector('.menu');
-            const options = dropDown.querySelector('.menu li');
+            const select = dropDown.querySelector('.select')!;
+            const caret = dropDown.querySelector('.caret')!;
+            const menu = dropDown.querySelector('.menu')!;
+            const options = dropDown.querySelectorAll('.menu li')!;
+            const selected = dropDown.querySelector('.selected')!;
 
             select.addEventListener('click', () => {
                 select.classList.toggle('select-clicked');
                 caret.classList.toggle('caret-rotate');
                 menu.classList.toggle('menu-open');
-            })
-        }
-        )
-    }*/
+            });
+            options.forEach(option => {
+                option.addEventListener('click', () => {
+                    selected.innerText = option.innerText;
+                    select.classList.remove('select-clicked');
+                    caret.classList.remove('caret-rotate');
+                    menu.classList.remove('menu-open');
+                    options.forEach(option => {
+                        option.classList.remove('active');
+                    });
+                    option.classList.add('active');
+                });
+            });
+        });
+    }
 
     return(
         <div className = "dropDown">
