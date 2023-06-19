ee737c1cf0eaa0babfec9c7e9d7bd1457cdf10d3
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-06-18 23:33:03 -0400

    sign

--- Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
+++ Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
@@ -6,24 +6,21 @@ export default function SignIn() {
   return (
     <div>
     <NavBar/>
-    <div className="sign">
-        <h1>Log In <i className="fa fa-address-card-o"> </i> </h1> 
-          <form>
-            <div className= "header">
-              RCSID:
-            </div>
-              <input type="text" placeholder='Username'/>
+      <div className='box'>
+        <div className="sign">
+          <h1>Log In <i className="fa fa-address-card-o"> </i> </h1> 
+            <form>
               <div className= "header">
-              Password:
+                RCSID:
               </div>
-                <input type="text" placeholder='Password'/>
-          </form>
-              <p className="forgotpass">
-                  Forgot your Password?
-              </p>
-          <button className= "signInbutt" type="submit" >Submit</button>
-
-
+                <input type="text" placeholder='Username'/>
+                <div className= "header">
+                Password:
+                </div>
+                  <input type="password" placeholder='Password'/>
+            </form>
+            <button className= "signInbutt" type="submit" >Submit</button>
+        </div>
       </div>
     </div>
 
