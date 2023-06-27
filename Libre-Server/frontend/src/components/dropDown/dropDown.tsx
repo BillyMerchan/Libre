9c8bd2ad27ad26636a06cdbef2bd45bfc8131a83
Rachel Tsui <rachelt3835@gmail.com>
2023-06-26 21:37:13 -0400

    dropdown

--- Libre-Server/frontend/src/components/dropDown/dropDown.tsx
+++ Libre-Server/frontend/src/components/dropDown/dropDown.tsx
@@ -1,41 +1,27 @@
-import React, {FC, useState} from 'react';
 import './dropDown.scss';
+import React, { useState, useEffect, useRef} from 'react';
 
+const DropDown = () => {
+    const [open, setOpen] = useState(false);
+    const menuRef = useRef(null);
 
-const [isExpanded, setisExpanded] = useState(false);
+    useEffect(() => {
+        const handler = (event: Event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)){
+                setOpen(false);
+            }
+        };
 
+    document.addEventListener('click', handler);
+    return () => {
+        document.removeEventListener('click', handler);
+    };
+    }, []);
 
-
-const DropDown: FC = () => {
-    {
-        const dropDowns = document.querySelectorAll('.dropDown');
-        dropDowns.forEach(dropDown => {
-            const select = dropDown.querySelector('.select')!;
-            const caret = dropDown.querySelector('.caret')!;
-            const menu = dropDown.querySelector('.menu')!;
-            const options = dropDown.querySelectorAll('.menu li')!;
-            const selected = dropDown.querySelector('.selected')!;
-
-            select.addEventListener('click', () => {
-                select.classList.toggle('select-clicked');
-                caret.classList.toggle('caret-rotate');
-                menu.classList.toggle('menu-open');
-            });
-            options.forEach(option => {
-                option.addEventListener('click', () => {
-                    selected.innerText = option.innerText;
-                    select.classList.remove('select-clicked');
-                    caret.classList.remove('caret-rotate');
-                    menu.classList.remove('menu-open');
-                    options.forEach(option => {
-                        option.classList.remove('active');
-                    });
-                    option.classList.add('active');
-                });
-            });
-        });
-    }
-
+    const toggleDropdown = () => {
+        open(!setOpen);
+      };
+    
     return(
         <div className = "dropDown">
             <div className = "select">
@@ -43,12 +29,13 @@ const DropDown: FC = () => {
                 <div className = "caret"></div>
             </div>
             <ul className = "menu">
-                <li className = "active">Option 1</li>
-                <li>Option 2</li>
+                <li className = "active">floor 1</li>
+                <li>floorr 2</li>
             </ul>
         </div>
-        )
-}
+        );
+    };
 
 
 export default DropDown;
+
