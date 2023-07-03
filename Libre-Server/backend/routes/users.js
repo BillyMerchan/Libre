6043376cd105725eddd45864b469becb19c57eeb
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-07-02 21:18:40 -0400

    User routes

---
+++ Libre-Server/backend/routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('./db/user');
+
+const router = express.Router();
+
+router.post('/signin', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ message: 'User not found' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            return res.status(401).json({ message: 'Password is Incorrect' });
+          }
+
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+module.exports = router;
