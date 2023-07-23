42e7c33e78a4e71be5ac237f62e2761f38b4cb16
Daniela Liang <artaffu@gmail.com>
2023-07-22 22:20:29 -0400

    nav, media query

--- Libre-Server/frontend/src/components/Button/Button.tsx
+++ Libre-Server/frontend/src/components/Button/Button.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
-const STYLES = ["btn--primary", "btn--outline"];
-const SIZES = ["btn--medium", 'btn--large'];
-
 interface Props {
     onClick?: () => void;
     buttonStyle?: string
