9e1343c728503b2e098d63a981334f5879ea3613
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-08-16 05:51:42 -0400

    FrontEnd Changes

--- Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
+++ Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SignIn.css";
+
+export default function SignIn() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function loginUser(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        alert(data.message);
+        navigate('/');
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <div className="sign">
+        <div className="Loginn">
+          <h1>Login</h1>
+          <form onSubmit={loginUser}>
+            <input className="InputL"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email"
+            />
+            <br />
+            <input className="InputL"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
+            <br />
+            <input className="buttL" type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
 import React from 'react'
 import NavBar from '../../components/NavBar/Navbar'
 import "./SignIn.css"
@@ -28,4 +94,4 @@ export default function SignIn() {
     </div>
 
   )
-}
+}*/
