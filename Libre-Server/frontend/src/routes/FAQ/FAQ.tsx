9538d7f29af9d67aad4379677e5af6aea3cd03f0
Daniela Liang <artaffu@gmail.com>
2023-09-19 17:46:22 -0400

    more faq edits

--- Libre-Server/frontend/src/routes/FAQ/FAQ.tsx
+++ Libre-Server/frontend/src/routes/FAQ/FAQ.tsx
@@ -5,17 +5,25 @@ import "./FAQ.css"
 export default function FAQ() {
   return (
     <div>
-    <NavBar/>
+      <NavBar />
       <div className='FAQ'>
+        <div className="background-img">
+
           <div className='title'>
             <h1>
-                Frequently Asked Questions
+              Frequently Asked Questions
             </h1>
           </div>
           <div className='QBox'>
-              Q: <span className='input'> Who created Libre? </span>
+            <div className='QandABlock'>
+              <span className='input'> Who created Libre? </span>
+            </div>
+            <div className='QandABlock'>
+              <span className='input'> How are rooms detected? </span>
+            </div>
           </div>
         </div>
+      </div>
     </div>
 
   )
