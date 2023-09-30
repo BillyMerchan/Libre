d3099cd5da17ce62f4ddec0dc4d79f9f7a57f5b5
JustinT9 <jus81818@yahoo.com>
2023-09-30 16:08:36 -0400

    some testing setup for signin page and other components

--- Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
+++ Libre-Server/frontend/src/routes/SignIn/SignIn.tsx
@@ -7,21 +7,21 @@ export default function SignIn() {
     <div>
     <NavBar/>
     <div className="sign">
-        <h1>Log In <i className="fa fa-address-card-o"> </i> </h1> 
+        <h1 data-testid="login">Log In <i className="fa fa-address-card-o"> </i> </h1> 
           <form>
-            <div className= "header">
+            <div className="header" data-testid="rcs">
               RCSID:
             </div>
               <input type="text" placeholder='Username'/>
-              <div className= "header">
+              <div className="header" data-testid="password">
               Password:
               </div>
                 <input type="text" placeholder='Password'/>
           </form>
-              <p className="forgotpass">
+              <p className="forgotpass" data-testid="forgotPassword">
                   Forgot your Password?
               </p>
-          <button className= "signInbutt" type="submit" >Submit</button>
+          <button className="signInbutt" type="submit" data-testid="submit">Submit</button>
 
 
       </div>
