e03a0a055b116a20cc7912dfad92be3bfcb45403
Daniela Liang <artaffu@gmail.com>
2023-10-17 17:50:19 -0400

    cleaned up code and change visual

--- Libre-Server/frontend/src/routes/map/map.tsx
+++ Libre-Server/frontend/src/routes/map/map.tsx
@@ -2,119 +2,95 @@ import { FC } from 'react';
 import './map.scss';
 import NavBar from '../../components/NavBar/Navbar';
 import DropDown from '../../components/dropDown/dropDown';
-import React, { useState } from "react";
-import {
-  room1, room2, room3, room4, room5, room6, room7
-} from '../../floors/floor3/index2';
 
 const Map2: FC = () => {
   return (
     <div>
       <NavBar />
-      <div className="Page">
-        <div className="Map">
-          <DB />
-        </div>
-      </div>
+      <DropDown />
+      <MapDiagram />
     </div>
   );
 }
 
-const DB: FC = () => {
-  return (
-    <div>
-      <DropDown />
-      <MapDiagram />
-    </div>
-  )
-}
-
 const MapDiagram: FC = () => {
   return (
-    <div className='Room'>
-      <div className="R2room332A">
-        <div className='text'>Room 322C</div>
+    <div className='Map'>
+      <div className="Room R332A">
+        <div className='text'>332A</div>
       </div>
-      <div className="R2room332B">
-        <div className='text'>Room 322B</div>
+      <div className="Room R332B">
+        <div className='text'>332B</div>
       </div>
-      <div className="R2room332C">
-        <div className='text'>Room 322C</div>
+      <div className="Room R332C">
+        <div className='text'>332C</div>
       </div>
-      <div className="R2room332D">
-        <div className='text'>Room 322C</div>
+      <div className="Room R332D">
+        <div className='text'>332D</div>
       </div>
-      <div className="R2room332E">
-        <div className='text'>Room 322C</div>
+      <div className="Room R332E">
+        <div className='text'>332E</div>
       </div>
-      <div className="R2room333">
-        <div className='text'>Room 322C</div>
+      <div className="Room R333">
+        <div className='text'>333</div>
       </div>
-
-
-      <div className="R2room337A">
-        <div className='text'>Room 322C</div>
+      <div className="Room R337A">
+        <div className='text'>337A</div>
       </div>
-      <div className="R2room337B">
-        <div className='text'>Room 322B</div>
+      <div className="Room R337B">
+        <div className='text'>337B</div>
       </div>
-      <div className="R2room337C">
-        <div className='text'>Room 322C</div>
+      <div className="Room R337C">
+        <div className='text'>337C</div>
       </div>
-      <div className="R2room337D">
-        <div className='text'>Room 322C</div>
+      <div className="Room R337D">
+        <div className='text'>337D</div>
       </div>
-      <div className="R2room337E">
-        <div className='text'>Room 322C</div>
+      <div className="Room R337E">
+        <div className='text'>337E</div>
       </div>
-
-      <div className="R2room342A">
-        <div className='text'>Room 322C</div>
+      <div className="Room R342A">
+        <div className='text'>342A</div>
       </div>
-      <div className="R2room342B">
-        <div className='text'>Room 322C</div>
+      <div className="Room R342B">
+        <div className='text'>342B</div>
       </div>
-      <div className="R2room342C">
-        <div className='text'>Room 322C</div>
+      <div className="Room R342C">
+        <div className='text'>342C</div>
       </div>
-
-      <div className="R2room352A">
-        <div className='text'>Room 322C</div>
+      <div className="Room R352A">
+        <div className='text'>352A</div>
       </div>
-      <div className="R2room352B">
-        <div className='text'>Room 322C</div>
+      <div className="Room R352B">
+        <div className='text'>352B</div>
       </div>
-      <div className="R2room352C">
-        <div className='text'>Room 322C</div>
+      <div className="Room R352C">
+        <div className='text'>352C</div>
       </div>
-      <div className="R2room352D">
-        <div className='text'>Room 322C</div>
+      <div className="Room R352D">
+        <div className='text'>352D</div>
       </div>
-      <div className="R2room352E">
-        <div className='text'>Room 322C</div>
+      <div className="Room R352E">
+        <div className='text'>352E</div>
       </div>
-      <div className="R2room353A">
-        <div className='text'>Room 322C</div>
+      <div className="Room R353A">
+        <div className='text'>353A</div>
       </div>
-      <div className="R2room353B">
-        <div className='text'>Room 322C</div>
+      <div className="Room R353B">
+        <div className='text'>353B</div>
       </div>
-
-      <div className="R2room311">
-        <div className='text'>Room 322C</div>
+      <div className="Room R311">
+        <div className='text'>311</div>
       </div>
-
-
-      <div className="R2room323C">
-        <div className='text'>Room 322C</div>
+      <div className="Room R323C">
+        <div className='text'>323C</div>
       </div>
-      <div className="R2room323B">
-        <div className='text'>Room 322C</div>
+      <div className="Room R323B">
+        <div className='text'>323B</div>
       </div>
-      <div className="R2room323A">
-        <div className='text'>Room 322C</div>
+      <div className="Room R323A">
+        <div className='text'>323A</div>
       </div>
-
     </div>
   )
 }
