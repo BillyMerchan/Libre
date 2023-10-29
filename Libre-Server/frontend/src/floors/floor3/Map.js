1062469a2409932c950be6a08a5ef4e472f6360e
Daniela Liang <artaffu@gmail.com>
2023-10-28 23:15:26 -0400

    map component display, hover

--- Libre-Server/frontend/src/floors/floor3/Map.js
+++ Libre-Server/frontend/src/floors/floor3/Map.js
@@ -1,261 +1,119 @@
-import * as React from "react";
-const SvgMap = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlSpace="preserve"
-    width={2180}
-    height={1320}
-    viewBox="0 0 576.792 349.25"
-    {...props}
-  >
-    <path
-      d="M38.122 90.951h15.492v9.834l8.775.031.034 15.727h-24.3Z"
-      style={{
-        display: "inline",
-        fill: "#bebebe",
-        stroke: "#000",
-        strokeWidth: 0.3,
-      }}
-    />
-    <path
-      d="M55.247 88.255h18.947v11.049H55.247z"
-      style={{
-        display: "inline",
-        fill: "#bebebe",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M143.051 306.936h27.171v26.849h-27.171zM214.147 209.636h17.291v25.345h-17.291zM201.241 236.629h30.984v30.984h-30.984zM322.897 237.237h27.187v17.922h-27.187zM429.743 307.419h18.902v26.258h-18.902z"
-      style={{
-        fill: "#bebebe",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M411.593 315.098h10.337l.03-7.595 7.085.024v26.043h-17.452zM449.611 307.5l7.053.061.035 7.645h10.122v18.337h-17.21Z"
-      style={{
-        fill: "#bebebe",
-        stroke: "#000",
-        strokeWidth: 0.3,
-      }}
-    />
-    <path
-      d="M508.115 88.318H527.1v11.087h-18.985z"
-      style={{
-        fill: "#bebebe",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M528.467 90.824h16.858v25.44h-25.63l.013-15.473 8.759-.132z"
-      style={{
-        fill: "#bebebe",
-        stroke: "#000",
-        strokeWidth: 0.3,
-      }}
-    />
-    <path
-      d="M301.102 80.041h20.124v9.265h-20.124z"
-      style={{
-        fill: "#bebebe",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M201.366 46.287h28.138v13.424h-28.138zM210.925 140.258h19.331v10.417h-19.331z"
-      style={{
-        fill: "#b5b5b5",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M429.743 22.096h19.009v26.026h-19.009z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.299596,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M428.916 22.103h-17.291v18.484H421.8l.054 7.562 7.061-.029z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.299241,
-      }}
-    />
-    <path
-      d="M411.593 41.294h9.451v17.022h-9.451zM457.311 41.273h9.493v16.783h-9.493z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M449.504 22.07h17.29V40.61H456.62l-.055 7.585-7.06-.029z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.299697,
-      }}
-    />
-    <path
-      d="M505.723 120.403h10.442v17.504h-10.442zM499.268 138.553h16.821v19.213h-16.821zM505.761 158.335h10.404v17.087h-10.404zM457.397 297.539h9.29V314.4h-9.29zM411.539 297.485h9.451v16.861h-9.451zM380.234 306.936h27.386v26.795h-27.386zM352.203 296.626h27.117v36.837h-27.117zM65.612 158.259h10.442v17.58H65.612zM65.645 138.513h16.888v19.116H65.645zM65.65 120.289h10.48v17.542H65.65zM160.664 41.32h9.585v16.888h-9.585z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M114.887 41.294h9.611v16.86h-9.611z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.301235,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M170.144 22.096h-17.259l-.064 26.154 7.108-.01.016-7.602h10.2z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.497372,
-        strokeDasharray: "none",
-        paintOrder: "markers stroke fill",
-      }}
-    />
-    <path
-      d="M133.117 22.1h18.92l-.043 26.15-18.931.015z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.5,
-        strokeDasharray: "none",
-        paintOrder: "markers stroke fill",
-      }}
-    />
-    <path
-      d="M114.979 22.1h17.312l.064 26.153-7.13-.01-.016-7.601h-10.23Z"
-      style={{
-        display: "inline",
-        opacity: 1,
-        fill: "#bfb",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.498124,
-        strokeDasharray: "none",
-        paintOrder: "markers stroke fill",
-      }}
-    />
-    <path
-      d="M292.33 208.562h27.278v57.134H292.33zM498.744 209.421h59.712v25.775h-59.712zM23.39 209.898h59.537v26.427H23.39z"
-      style={{
-        display: "inline",
-        fill: "#d5fafc",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M210.925 151.642h19.653v26.956h-19.653zM211.891 118.672h16.539v19.331h-16.539z"
-      style={{
-        display: "inline",
-        fill: "#e9f8a4",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-        strokeDasharray: "none",
-        strokeDashoffset: 0,
-        strokeOpacity: 1,
-      }}
-    />
-    <path
-      d="M290.773 16.861h29.802v26.107l-19.443-.037-.144-13.404-1.102.025-.093 17.177-9.02.036z"
-      style={{
-        display: "inline",
-        fill: "#e9f8a4",
-        stroke: "#000",
-        strokeWidth: 0.3,
-      }}
-    />
-    <path
-      d="M301.178 71.84h12.948v-1.037l-13.02-.14.087-11.633 6.251-.05-.014-11.546 13.53-.01.19 31.554h-19.972z"
-      style={{
-        display: "inline",
-        fill: "#e9f8a4",
-        fillOpacity: 1,
-        stroke: "#000",
-        strokeWidth: 0.3,
-      }}
-    />
-  </svg>
-);
-export default SvgMap;
+const MapDiagram = ({ onClick }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      viewBox="0 0 576.792 349.25"
+    >
+      <path
+        id="337B" className="studyRoom" onClick={() => onClick('337B')} 
+        d="M428.916 22.103h-17.291v18.484H421.8l.054 7.562 7.061-.029z"
+      />
+      <path
+        id="337A" className="studyRoom" onClick={() => onClick('337A')} 
+        d="M411.593 41.294v17.022h9.45v-17.022h-9.45"
+        style={{
+        }}
+      />
+      <path
+        id="337E" className="studyRoom" onClick={() => onClick('337E')} 
+        d="M457.311 41.273v16.783h9.492v-16.783h-9.492"
+      />
+      <path
+        id="337D" className="studyRoom" onClick={() => onClick('337D')} 
+        d="M449.504 22.07h17.29V40.61H456.62l-.055 7.585-7.06-.029z"
+      />
+      <path
+        id="342A" className="studyRoom" onClick={() => onClick('342A')} 
+        d="M505.723 120.403v17.504h10.442v-17.504h-10.442"
+      />
+      <path
+        id="342B" className="studyRoom" onClick={() => onClick('342B')} 
+        d="M499.268 138.553v19.212h16.82v-19.212h-16.82"
+      />
+      <path
+        id="342C" className="studyRoom" onClick={() => onClick('342C')} 
+        d="M505.76 158.335v17.087h10.405v-17.087h-10.405"
+      />
+      <path
+        id="352A" className="studyRoom" onClick={() => onClick('352A')} 
+        d="M457.397 297.54v16.86h9.29v-16.86h-9.29"
+      />
+      <path
+        id="352E" className="studyRoom" onClick={() => onClick('352E')} 
+        d="M411.54 297.485v16.861h9.45v-16.861h-9.45"
+      />
+      <path
+        id="353A" className="studyRoom" onClick={() => onClick('353A')} 
+        d="M380.234 306.936v26.795h27.385v-26.795h-27.385"
+      />
+      <path
+        id="353B" className="studyRoom" onClick={() => onClick('353B')} 
+        d="M352.203 296.626v36.837h27.118v-36.837h-27.118"
+      />
+      <path
+        id="323A" className="studyRoom" onClick={() => onClick('323A')} 
+        d="M65.612 158.26v17.58h10.442v-17.58h-10.442"
+      />
+      <path
+        id="323B" className="studyRoom" onClick={() => onClick('323B')} 
+        d="M65.645 138.513v19.117h16.888v-19.117h-16.888"
+      />
+      <path
+        id="323C" className="studyRoom" onClick={() => onClick('323C')} 
+        d="M65.65 120.289h10.48v17.542H65.65Z"
+      />
+      <path
+        id="332E" className="studyRoom" onClick={() => onClick('332E')}
+        d="M160.664 41.32h9.585v16.888h-9.585z"
+      />
+      <path
+        id="332A" className="studyRoom" onClick={() => onClick('room1')} 
+        d="M114.887 41.294h9.61v16.86h-9.61z"
+      />
+      <path
+        id="332D" className="studyRoom" onClick={() => onClick('332D')} 
+        d="M170.144 22.096h-17.259l-.064 26.154 7.108-.01.016-7.602h10.2z"
+      />
+      <path
+        id="332C" className="studyRoom" onClick={() => onClick('332C')}
+        d="M133.117 22.1h18.92l-.043 26.15-18.931.015z"
+      />
+      <path
+        id="332B" className="studyRoom" onClick={() => onClick('332B')} 
+        d="M114.979 22.1h17.312l.064 26.153-7.13-.01-.016-7.601h-10.23Z"
+      />
+      <path
+        id="stairsMain" className="stairs" onClick={() => onClick('stairsMain')} 
+        d="M292.33 208.562v57.134h27.279v-57.134h-27.279"
+      />
+      <path
+        id="stairsRight" className="stairs" onClick={() => onClick('stairsRight')} 
+        d="M498.744 209.421v25.775h59.712v-25.775h-59.712"
+      />
+
+      <path
+        id="stairsLeft" className="stairs" onClick={() => onClick('stairsLeft')}
+        d="M23.39 209.898v26.427h59.537v-26.427h-59.537"
+      />
+      <path
+        id="copyRoom" className="utilities" onClick={() => onClick('copyRoom')} 
+        d="M210.925 151.642h19.653v26.958h-19.653z"
+      />
+
+      <path
+        id="elevator" className="utilities" onClick={() => onClick('elevator')} 
+        d="M211.891 118.672h16.54v19.331h-16.54z"
+      />
+      <path
+        id="menbathroom" className="bathroom" onClick={() => onClick('menbathroom')} 
+        d="M290.773 16.861h29.802v26.107l-19.443-.037-.144-13.404-1.102.025-.093 17.177-9.02.036z"
+      />
+      <path
+        id="womenbathroom" className="bathroom" onClick={() => onClick('womenbathroom')} 
+        d="M301.178 71.84h12.948v-1.037l-13.02-.14.087-11.633 6.251-.05-.014-11.546 13.53-.01.19 31.554h-19.972z"
+      />
+    </svg>
+  );
+}
+export default MapDiagram
--- Libre-Server/frontend/src/routes/map/map.tsx
+++ Libre-Server/frontend/src/routes/map/map.tsx
@@ -2,101 +2,30 @@ import { FC } from 'react';
 import './map.scss';
 import NavBar from '../../components/NavBar/Navbar';
 import DropDown from '../../components/dropDown/dropDown';
-// import { ReactComponent as Map } from '../../floors/floor3/map.svg';
-import Map from '../../floors/floor3/Map.js';
-// const map = require("../../floors/floor3/map2.svg") as string;
+import MapDiagram from '../../floors/floor3/Map.js';
 
-const Map2: FC = () => {
+const handleClick = (id: string) => {
+  console.log(`Clicked room: ${id}`);
+
+  // implement popup with desc and room id
+
+};
+
+
+const Map: FC = () => {
   return (
     <div>
       <NavBar />
-      <Map />
-      <DropDown />
-      <MapDiagram />
+
+      <div className="Map">
+        <DropDown />
+        <div className="MapDiagramContainer">
+          <MapDiagram onClick={handleClick} />
+        </div>
+      </div>
+
     </div>
   );
 }
 
-const MapDiagram: FC = () => {
-  return (
-    <div className='Map'>
-      <div className="Room R332A">
-        <div className='text'>332A</div>
-      </div>
-      <div className="Room R332B">
-        <div className='text'>332B</div>
-      </div>
-      <div className="Room R332C">
-        <div className='text'>332C</div>
-      </div>
-      <div className="Room R332D">
-        <div className='text'>332D</div>
-      </div>
-      <div className="Room R332E">
-        <div className='text'>332E</div>
-      </div>
-      <div className="Room R333">
-        <div className='text'>333</div>
-      </div>
-      <div className="Room R337A">
-        <div className='text'>337A</div>
-      </div>
-      <div className="Room R337B">
-        <div className='text'>337B</div>
-      </div>
-      <div className="Room R337C">
-        <div className='text'>337C</div>
-      </div>
-      <div className="Room R337D">
-        <div className='text'>337D</div>
-      </div>
-      <div className="Room R337E">
-        <div className='text'>337E</div>
-      </div>
-      <div className="Room R342A">
-        <div className='text'>342A</div>
-      </div>
-      <div className="Room R342B">
-        <div className='text'>342B</div>
-      </div>
-      <div className="Room R342C">
-        <div className='text'>342C</div>
-      </div>
-      <div className="Room R352A">
-        <div className='text'>352A</div>
-      </div>
-      <div className="Room R352B">
-        <div className='text'>352B</div>
-      </div>
-      <div className="Room R352C">
-        <div className='text'>352C</div>
-      </div>
-      <div className="Room R352D">
-        <div className='text'>352D</div>
-      </div>
-      <div className="Room R352E">
-        <div className='text'>352E</div>
-      </div>
-      <div className="Room R353A">
-        <div className='text'>353A</div>
-      </div>
-      <div className="Room R353B">
-        <div className='text'>353B</div>
-      </div>
-      <div className="Room R311">
-        <div className='text'>311</div>
-      </div>
-      <div className="Room R323C">
-        <div className='text'>323C</div>
-      </div>
-      <div className="Room R323B">
-        <div className='text'>323B</div>
-      </div>
-      <div className="Room R323A">
-        <div className='text'>323A</div>
-      </div>
-    </div>
-  )
-}
-
-export default Map2;
+export default Map;
