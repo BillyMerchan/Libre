d82d874053da89a83c11f06a19afe826880e4ef1
Daniela Liang <artaffu@gmail.com>
2023-11-07 19:14:56 -0500

    onclick popup

--- Libre-Server/frontend/src/routes/map/map.tsx
+++ Libre-Server/frontend/src/routes/map/map.tsx
@@ -1,18 +1,24 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './map.scss';
 import NavBar from '../../components/NavBar/Navbar';
 import DropDown from '../../components/dropDown/dropDown';
 import MapDiagram from '../../floors/floor3/Map.js';
-
-const handleClick = (id: string) => {
-  console.log(`Clicked room: ${id}`);
-
-  // implement popup with desc and room id
-
-};
-
+import RoomPopup from '../../components/RoomPopup/RoomPopup'; 
 
 const Map: FC = () => {
+  const [popupData, setPopupData] = useState({ roomName: '', description: '', color: '' });
+  const [isPopupVisible, setPopupVisibility] = useState(false);
+
+  // NOTE: for description, in future we can add images/desc in Map.js. Once hardware is done, also pass in occupancy data
+  const handleClick = (id: string, fill: string) => {
+    setPopupData({ roomName: `Room: ${id}`, description: 'Description of the room.', color: `${fill}` });
+    setPopupVisibility(true);
+  };
+
+  const handleClosePopup = () => {
+    setPopupVisibility(false);
+  };
+
   return (
     <div>
       <NavBar />
@@ -24,8 +30,9 @@ const Map: FC = () => {
         </div>
       </div>
 
+      {isPopupVisible && <RoomPopup roomName={popupData.roomName} description={popupData.description} color={popupData.color} onClose={handleClosePopup} />}
     </div>
   );
-}
+};
 
 export default Map;
