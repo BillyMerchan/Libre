058e23edd55fb4138e4ab8a96bec4afa436cab03
Billy Merchan <billymerchan@gmail.com>
2023-12-08 02:11:18 -0500

    footer-css updates

--- Libre-Server/frontend/src/components/Footer/Footer.tsx
+++ Libre-Server/frontend/src/components/Footer/Footer.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import "./Footer.scss"
+import ghscImg from "../../img/ghsc.png";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="footer-content">
-          <p>This is an RCOS project at RPI. </p>
-          <p>
+        <div className="footer-content copyright-content">
+        <p className = "copyright"> ©2022-2023 Libre </p>
+        </div>
+        <div className="footer-content link-content">
             <a href="https://github.com/BillyMerchan/Libre" target="_blank" rel="noopener noreferrer">
-              Check out our GitHub Repository
+            <img className="ghlogo" src={ghscImg} alt="Github Logo"/>
             </a>
-          </p>
         </div>
       </div>
     </footer>
